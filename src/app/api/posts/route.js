876372a7ad1fff82933e2221224cc5602f82585c
seafood <mail.seafood@proton.me>
2024-03-03 01:42:11 +0300

    Add revalidation to /posts endpoint

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,5 +1,6 @@
 import { Post } from "@/lib/models";
 import { connectToDb } from "@/lib/utils";
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
 
 export const GET = async (request) => {
@@ -7,6 +8,7 @@ export const GET = async (request) => {
     connectToDb();
 
     const posts = await Post.find();
+    revalidatePath("/posts");
     return NextResponse.json(posts);
   } catch (err) {
     console.log(err);
