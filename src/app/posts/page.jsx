a7832f895833c18146c01c098428f5ae4e0aab7f
seafood <mail.seafood@proton.me>
2024-03-03 04:28:21 +0300

    use api for fetching posts again (idk why)

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,27 +1,26 @@
 import PostCard from "@/components/postCard/postCard";
 import styles from "./posts.module.css";
-import { getPosts } from "@/lib/data";
 //import { revalidatePath } from "next/cache";
 
 // FETCH DATA WITH AN API
-// const getData = async () => {
-//   const res = await fetch(`${process.env.URL}/api/posts`, {
-//     next: { revalidate: 1 },
-//   });
+const getData = async () => {
+  const res = await fetch(`${process.env.URL}/api/posts`, {
+    next: { revalidate: 1 },
+  });
 
-//   if (!res.ok) {
-//     throw new Error("Something went wrong");
-//   }
-//   //revalidatePath("/posts");
-//   return res.json();
-// };
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+  //revalidatePath("/posts");
+  return res.json();
+};
 
 const BlogPage = async () => {
   // FETCH DATA WITH AN API
-  // const posts = await getData();
+  const posts = await getData();
 
   // FETCH DATA WITHOUT AN API
-  const posts = await getPosts();
+  // const posts = await getPosts();
 
   return (
     <div className={styles.container}>
