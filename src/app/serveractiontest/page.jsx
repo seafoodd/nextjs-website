0b60b01a5f5a722156b5497af354dbd569264c91
seafood <mail.seafood@proton.me>
2024-03-06 10:00:33 +0000

    update serveractiontest page

--- src/app/serveractiontest/page.jsx
+++ src/app/serveractiontest/page.jsx
@@ -1,22 +1,11 @@
+import PostForm from "@/components/postForm/postForm";
 import { addPost } from "@/lib/actions";
 import { deletePost } from "@/lib/actions";
 
 const ServerActionTestPage = () => {
   return (
     <div>
-      <form action={addPost}>
-        <input type="text" placeholder="title" name="title" />
-        <input type="text" placeholder="desc" name="desc" />
-        <input type="text" placeholder="slug" name="slug" />
-        <input type="text" placeholder="userId" name="userId" />
-        <input type="text" placeholder="img" name="img" />
-        <button>Create post</button>
-      </form>
-
-      <form action={deletePost}>
-        <input type="text" placeholder="postId" name="id" />
-        <button>Delete post</button>
-      </form>
+      <PostForm userId={session.user.id} />
     </div>
   );
 };
