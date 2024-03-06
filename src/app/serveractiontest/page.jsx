7f87330155a5bfd7ddb49f93c7e925758149be00
seafood <mail.seafood@proton.me>
2024-03-06 10:05:53 +0000

    actually fixed "session not defined"

--- src/app/serveractiontest/page.jsx
+++ src/app/serveractiontest/page.jsx
@@ -3,7 +3,9 @@ import { addPost } from "@/lib/actions";
 import { deletePost } from "@/lib/actions";
 import { auth } from "@/lib/auth";
 
-const ServerActionTestPage = () => {
+const ServerActionTestPage = async () => {
+  const session = await auth();
+
   return (
     <div>
       <PostForm userId={session.user.id} />
